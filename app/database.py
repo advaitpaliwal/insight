3df499d5c9c5b310ec220838f1fb1e8b00d02bec
Advait Paliwal <advaitspaliwal@gmail.com>
2024-04-14 01:10:21 -0400

    fix db import

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from src.config import FIREBASE_CREDENTIALS_FILE
+from app.config import FIREBASE_CREDENTIALS_FILE
 
 
 class FirestoreDB:

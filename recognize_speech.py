385a93a1539a95cd0389f96d0737bc1bc8fbb802
Advait Paliwal <advaitspaliwal@gmail.com>
2024-04-14 10:47:54 -0400

    rm speak

--- recognize_speech.py
+++ recognize_speech.py
@@ -31,8 +31,8 @@ def recognize_speech(endpoint_duration_sec=1.0):
             enable_automatic_punctuation=True
         )
         print("One moment please...")
-        pa = pyaudio.PyAudio()
-        speak(random.choice(listening_phrases))
+        # pa = pyaudio.PyAudio()
+        # speak(random.choice(listening_phrases))
         print("Listening...")
         audio_stream = pa.open(
             rate=cheetah.sample_rate,

bb4aab679ddaf0476d8722a2d2e81dd9ce098fa8
Alexandra Enders <endersa@umich.edu>
2024-04-14 00:05:32 -0400

    lights

--- pi/lights.py
+++ pi/lights.py
@@ -1,32 +1,32 @@
-from rpi_ws281x import PixelStrip, Color
 import time
+from rpi_ws281x import PixelStrip, Color
 
-# Configuration (make these numbers larger if unsure)
-LED_COUNT = 300          # Number of LED pixels (set this higher than you expect your strip to be)
-LED_PIN = 18             # GPIO pin connected to the pixels (must support PWM)
-LED_FREQ_HZ = 800000     # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10             # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 20      # Set to a low brightness
-LED_INVERT = False       # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 0          # set to '1' for GPIOs 13, 19, 41, 45 or 53
+# LED strip configuration:
+LED_COUNT = 10          # Number of LED pixels - change this to the number of pixels in your strip
+LED_PIN = 18            # GPIO pin connected to the pixels (18 uses PWM)
+LED_FREQ_HZ = 800000    # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10            # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255    # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False      # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0         # Set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-# Initialize the library (strip)
 strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 strip.begin()
 
-def test_leds(strip):
-    """Light up each LED in sequence to find the last one."""
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
     for i in range(strip.numPixels()):
-        strip.setPixelColor(i, Color(255, 0, 0))  # Red color
+        strip.setPixelColor(i, color)
         strip.show()
-        time.sleep(0.1)
-        strip.setPixelColor(i, Color(0, 0, 0))  # Turn off after testing
-        strip.show()
+        time.sleep(wait_ms / 1000.0)
 
 try:
-    test_leds(strip)
+    while True:
+        print('Color wipe animations.')
+        colorWipe(strip, Color(255, 0, 0))  # Red wipe
+        colorWipe(strip, Color(0, 255, 0))  # Green wipe
+        colorWipe(strip, Color(0, 0, 255))  # Blue wipe
+        colorWipe(strip, Color(0, 0, 0))    # Off wipe
+        time.sleep(1)
 except KeyboardInterrupt:
-    # Turn off all LEDs
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, Color(0, 0, 0))
-    strip.show()
+    colorWipe(strip, Color(0, 0, 0), 10)

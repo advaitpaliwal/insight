3201460e757c364370c45382610014a607cc97bd
Advait Paliwal <advaitspaliwal@gmail.com>
2024-04-14 00:19:42 -0400

    small fix

--- src/service.py
+++ src/service.py
@@ -6,7 +6,7 @@ from storage import GCStorage
 from datetime import datetime
 from uuid import uuid4
 from google.ai.generativelanguage import Content, Part, FileData
-from pi.camera import take_picture
+from src.camera import take_picture
 from google.generativeai.types.file_types import File
 import tempfile
 

7f7390db43a4d28adf3f5298dcd3ddf42c83deec
Advait Paliwal <advaitspaliwal@gmail.com>
2024-04-14 08:42:53 -0400

    undo

--- recognize_speech.py
+++ recognize_speech.py
@@ -3,10 +3,7 @@ import speech_recognition as sr
 
 def recognize_speech():
     recognizer = sr.Recognizer()
-    mic = sr.Microphone(device_index=2)
-    names = sr.Microphone.list_microphone_names()
-    print(names)
-    with mic as source:
+    with sr.Microphone() as source:
         print("Listening...")
         audio = recognizer.listen(source)
     try:
